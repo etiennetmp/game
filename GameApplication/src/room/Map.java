a75530ce39a6a1313b28639c6103463cc17dc705
Alexandre Moreau <alexandre1.moreau@epita.fr>
2019-03-20 15:19:28 +0000

    m

--- GameApplication/src/room/Map.java
+++ GameApplication/src/room/Map.java
@@ -30,9 +30,12 @@ public class Map {
         mapList[1] = new Room("farm", 1, false, false, "Here is a farm!");
         mapList[2] = new Room("village exit", 2, false, false, "There is a clear land in front of you. Or maybe would you like to attempt the dungeon...");
         mapList[3] = new Room("dungeon entry", 1, false, true, "You are in the dungeon.. Can't go back ! There is an old man in the ground and a door in front of you.");
-        mapList[4] = new Room("first room", 1, false, false, "There is a very dark hole.. seems very deep.. Shall you continue or explore ?");
-        mapList[5] = new Room("dark room", 1, false, false, "It's very dark in here.. You are in a coal reserve.. You feel something with your feet..");
-        //mapList[6] = new Room("statue room", 2, )
+        mapList[4] = new Room("first room", 2, false, false, "There is a very dark hole.. seems very deep.. Shall you continue or explore ?");
+        mapList[5] = new Room("dark room", 0, false, false, "It's very dark in here.. You are in a coal reserve.. You feel something with your feet..");
+        mapList[6] = new Room("statue room", 1, false, false, "This room is giant!! There is a statue in the center with diamonds incrusted into it's eyes! The statue is very high!");
+        mapList[7] = new Room("small room", 1, false, false, "You found glasses on the ground.");
+        mapList[8] = new Room("final boss room", 1, false, false, "Final boss !!!!!");
+        
         mapList[19] = new Room("fields", 1, false, false, "Here is the open world!");
         
         linkAll();
@@ -41,15 +44,15 @@ public class Map {
     
     private void linkAll() {
         /*  has to be done manualy :'(  */
-        backLinkRoom(0, 0);
+        mapList[0].prec = mapList[0];
         linkRoom(0, 1);
-        backLinkRoom(1, 0);
         linkRoom(1, 2);
-        backLinkRoom(2, 1);
         linkRooms(2, 3, 19);
-        backLinkRoom(3, 2);
-        backLinkRoom(19, 2);
-        linkRoom(3, 3);
+        linkRoom(3, 4);
+        linkRooms(4, 5, 6);
+        linkRoom(6, 7);
+        linkRoom(7, 8);
+        linkRoom(8, 2);
         linkRoom(19, 19);
     }
     
@@ -60,6 +63,7 @@ public class Map {
 
             if (mapList[src].adj[i] == null) {
                 mapList[src].adj[i] = mapList[dst];
+                mapList[dst].prec = mapList[src];
                 break;
             }
         }
@@ -72,14 +76,11 @@ public class Map {
             if (mapList[src].adj[i] == null) {
                 mapList[src].adj[i] = mapList[dst1];
                 mapList[src].adj[i+1] = mapList[dst2];
+                mapList[dst1].prec = mapList[src];
+                mapList[dst2].prec = mapList[src];
                 break;
             }
         }
-    }
-    
-    
-    private void backLinkRoom(int src, int dst) {
-        mapList[src].prec = mapList[dst];
     }
     
     /**
